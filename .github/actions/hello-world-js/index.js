7146d367fcd17d93c1296419b130722c2045d342
odonML <odon_ml@outlook.com>
2026-03-19 22:38:10 -0600

    types actions

---
+++ .github/actions/hello-world-js/index.js
@@ -0,0 +1,4 @@
+//.github/actions/hello-world-js/index.js
+const core = require('@actions/core');
+const nameToGreet = core.getInput('who-to-greet');
+console.log(`Hola ${nameToGreet}! Estás un paso más cerca de ser DevOps.`);
