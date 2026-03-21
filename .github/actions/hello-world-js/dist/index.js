a6ca60e2ea0f06128a7589661d5a7a07fafe2d64
odonML <odon_ml@outlook.com>
2026-03-21 13:54:14 -0600

    change compiler js to rollup

--- .github/actions/hello-world-js/dist/index.js
+++ .github/actions/hello-world-js/dist/index.js
@@ -1,57 +1,25 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
+import require$$0 from '@actions/core';
 
-/***/ 761:
-/***/ ((module) => {
+function getDefaultExportFromCjs (x) {
+	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
+}
 
-module.exports = eval("require")("./@actions/core");
+var src = {};
 
+var hasRequiredSrc;
 
-/***/ })
+function requireSrc () {
+	if (hasRequiredSrc) return src;
+	hasRequiredSrc = 1;
+	//.github/actions/hello-world-js/index.js
+	const core = require$$0;
+	const nameToGreet = core.getInput('who-to-greet');
+	console.log(`Hola ${nameToGreet}! Estás un paso más cerca de ser DevOps.`);
+	return src;
+}
 
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __nccwpck_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		var threw = true;
-/******/ 		try {
-/******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
-/******/ 			threw = false;
-/******/ 		} finally {
-/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
-/******/ 		}
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/compat */
-/******/ 	
-/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
-/******/ 	
-/************************************************************************/
-var __webpack_exports__ = {};
-//.github/actions/hello-world-js/index.js
-const core = __nccwpck_require__(761);
-const nameToGreet = core.getInput('who-to-greet');
-console.log(`Hola ${nameToGreet}! Estás un paso más cerca de ser DevOps.`);
-module.exports = __webpack_exports__;
-/******/ })()
-;
+var srcExports = requireSrc();
+var index = /*@__PURE__*/getDefaultExportFromCjs(srcExports);
+
+export { index as default };
+//# sourceMappingURL=index.js.map
