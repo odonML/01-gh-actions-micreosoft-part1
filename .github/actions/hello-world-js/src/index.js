7f90dba913773ec4bd8a094b708a8c159a821531
odonML <odon_ml@outlook.com>
2026-03-21 13:56:27 -0600

    change importation js library action/core

--- .github/actions/hello-world-js/src/index.js
+++ .github/actions/hello-world-js/src/index.js
@@ -1,4 +1,4 @@
 //.github/actions/hello-world-js/index.js
-const core = require('@actions/core');
+import * as core from "@actions/core";
 const nameToGreet = core.getInput('who-to-greet');
 console.log(`Hola ${nameToGreet}! Estás un paso más cerca de ser DevOps.`);
